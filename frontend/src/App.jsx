3103114e2ea67a5d061af556552db5810065b2e9
Yam25 <yaminitae30@gmail.com>
2026-05-24 21:17:11 +0530

    feat: add Prettier configuration and format code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   return (
     <>
       <h1>Cortex</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
