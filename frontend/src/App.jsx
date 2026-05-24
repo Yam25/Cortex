902d1ce1c71f5f118c9436b488b452e4f7b3d701
Yam25 <yaminitae30@gmail.com>
2026-05-24 23:22:25 +0530

    feat: implement LeftPanel component and sidebar functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
 import './App.css';
+import LeftPanel from './components/LeftPanel';
+import { useState } from 'react';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <>
-      <h1>Cortex</h1>
+      {isSidebarOpen ? (
+        <LeftPanel onCollapse={() => setIsSidebarOpen(false)} />
+      ) : (
+        <button className="sidebar-open-btn" onClick={() => setIsSidebarOpen(true)}>
+          <i className="fa-solid fa-angles-right"></i>
+        </button>
+      )}
     </>
   );
 }
---
+++ frontend/src/components/LeftPanel.jsx
@@ -0,0 +1,37 @@
+import './LeftPanel.css';
+
+function LeftPanel({ onCollapse }) {
+  return (
+    <div className="left-panel">
+      <div className="left-header">
+        <div className="left-brand">
+          <img src="logo.png" alt="cortex-logo" className="logo" />
+
+          <div className="brand-text">
+            <h1 className="title">Cortex</h1>
+            <p className="sub-title">Your second brain.</p>
+          </div>
+        </div>
+
+        <span className="left-collapse" onClick={onCollapse}>
+          <i className="fa-solid fa-angles-left"></i>
+        </span>
+      </div>
+
+      <button className="new-chat-btn">
+        <i className="fa-solid fa-pen-to-square"></i>
+        <span>New Chat</span>
+      </button>
+
+      <div className="profile-footer">
+        <div className="profile-avatar">Y</div>
+        <div className="profile-text">
+          <p className="profile-name">Yamini</p>
+          <p className="profile-subtitle">Your memory workspace</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftPanel;
